f565a9e2ded71a038280f467f97a4e42bf1075e4
Andrea <andreatp11@gmail.com>
2023-06-12 11:09:56 +0200

    Sequelize Setup

---
+++ models/plans.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Plans extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Plans.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      keyword: DataTypes.STRING,
+    },
+    {
+      image: DataTypes.BLOB,
+    },
+    {
+      sequelize,
+      modelName: "Plans",
+    }
+  );
+  return Plans;
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
+// Get user by username
+
+// Get all plans
+
+// Post selection (every time you click YES the userID and planID goes to selection)
+
+// Get selection by planID where planID count is 2(twice)  << do this every time the user says yes
+
+// Delete all from selections (happens when you click cancel or when you match)
+
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
