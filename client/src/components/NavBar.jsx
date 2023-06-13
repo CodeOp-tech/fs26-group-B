224076fba115de4cfaabef52d199580a98c9666e
Daggiana Madrid Andrade <daggianama@gmail.com>
2023-06-13 15:59:18 +0200

    installed material ui added styles.js for themes

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import { Link  } from 'react-router-dom'
 // import Menu from './Menu'
 // import Api from '../Api'
+import { ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import {theme, nav_bar} from '../styles'
 
 
 
 export default function NavBar() {
     // const auth = useContext(AuthContext);
+  
     const auth = true;
     const [selectSignUp, setSelectSignUp] = useState(false)
 
@@ -18,21 +22,28 @@ export default function NavBar() {
     }
 
 
-    return (
-        <div className="navBar">
-            <div className="navBar__logo">
-                <h1>Logo</h1>
-            </div>
-            <div className="navBar__links">
-                {!auth && selectSignUp === false && <Link to={"/register"} onClick={handleSelectSignUp}>Sign Up</Link>}
-                {!auth && selectSignUp === true && <Link to="/login" onClick={() => setSelectSignUp(false)}>Login</Link>}
-                <button>Logout</button>
-                <button>menu</button>
-            </div>
-        </div>
-    )
-
-
-
     
+  return (
+    <div>
+      <div className="navBar__logo">
+        <h1>Logo</h1>
+      </div>
+      <ThemeProvider theme={theme}>
+        <Container sx={nav_bar}>
+          {!auth && selectSignUp === false && (
+            <Link to="/register" onClick={handleSelectSignUp}>
+              Sign Up
+            </Link>
+          )}
+          {!auth && selectSignUp === true && (
+            <Link to="/login" onClick={() => setSelectSignUp(false)}>
+              Login
+            </Link>
+          )}
+          <button>Logout</button>
+          <button>menu</button>
+        </Container>
+      </ThemeProvider>
+    </div>
+  );
 }
