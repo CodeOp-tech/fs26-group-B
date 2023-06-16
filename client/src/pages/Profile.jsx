4b129d209bd9ec37453dc0e2d7e0c2ff33775e9d
the-swedish-one <a.damgaard@oxon.org>
2023-06-16 15:09:13 +0200

    Add function to request user data

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    requestData();
+  }, []);
+
+  const requestData = async () => {
+    try {
+      const { data } = await axios("/api/auth/profile", {
+        headers: {
+          authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      setData(error.message);
+    }
+  };
+
+  return <div>Profile: {data?.message} </div>;
 }
