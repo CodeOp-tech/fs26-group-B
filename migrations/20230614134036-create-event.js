e5693728acb4f31ca0ce9d8be2d39699bf11edb2
the-swedish-one <a.damgaard@oxon.org>
2023-06-16 17:38:47 +0200

    Update Event migration to singular

--- migrations/20230614134036-create-event.js
+++ migrations/20230614134036-create-event.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Events", {
+    await queryInterface.createTable("Event", {
       id: {
         allowNull: false,
         autoIncrement: true,
