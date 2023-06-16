0dccee3553605ad2c7b2ec84d4c5715995781747
the-swedish-one <a.damgaard@oxon.org>
2023-06-16 09:50:09 +0200

    Add placeholder list of endpoints needed

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,5 +1,39 @@
-import axios from "axios"
+import axios from "axios";
+
+// Users
 
 export const getUser = async (userId) => {
-  await axios.get(`/users/${userId}`)
+  await axios.get(`/users/${userId}`);
 };
+
+/* 
+getUser(username)
+
+
+
+
+
+// Plans
+
+getAllPlans()
+
+getPlan(planId)
+
+
+
+
+// Events
+
+createEvent(inviter, invitee)
+
+getEvent(hash)
+
+
+
+
+// Selections
+
+addSelection(eventId, userId, planId)
+
+
+*/
