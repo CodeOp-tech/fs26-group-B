2ed0081c7129d988cdd21230550f976bb223689c
Daggiana Madrid Andrade <daggianama@gmail.com>
2023-06-16 10:51:51 +0200

    adjusting merge conflicts

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -5,10 +5,23 @@ import SendIcon from "@mui/icons-material/Send";
 import { useNavigate } from "react-router-dom";
 import Select from "react-select";
 
+// USER FLOW
+
+// 1. user1 search for user2
+// 2. is its found, user1 have the option of sending invitation to user2
+// 3. user1 press invitation button and back end register a new event with user 1 and user2 and send back the new event hash
+// 4. user1 receive invitation confirmation and a option to start the selection
+// 5. user1 press start and is redirect to selection/event page with the hash of the event
+
+//// notification page would check on events table if there is an open event for the user and if there is, it would show the invitation there
+
+// 6. user2 login and sees a notification of a new invitation
+// 7. user2 press the notification and has the option to open the invitation
+// 8. user2 press accept and is redirect to selection/event page with the hash of the event
+
 export default function Search() {
 	const [userSearch, setUserSearch] = useState("");
 	const [user, setUser] = useState("");
-
 	const navigate = useNavigate();
 	const [errorMsg, setErrorMsg] = useState(false);
 	const [invitationMsg, setInvitationMsg] = useState("");
@@ -17,7 +30,6 @@ export default function Search() {
 	useEffect(() => {
 		setUser({});
 	}, []);
-
 
 	const handleSearch = (e) => {
 		e.preventDefault();
@@ -29,13 +41,10 @@ export default function Search() {
 	const searchUser = async () => {
 		try {
 			const data = await api.getUsername(userSearch);
-
-
 			(data && setUser(data)) || setErrorMsg(true);
 			data && setErrorMsg(false);
 			console.log(errorMsg);
 			console.log(data);
-
 		} catch (error) {
 			console.log(error);
 		}
@@ -58,13 +67,11 @@ export default function Search() {
 		// navigate(`/event/${eventHash}`) with real api call
 	};
 
-
 	const handleChange = (e) => {
 		setUserSearch(e.target.value);
 	};
 
 	return (
-
 		<div>
 			<div className="search-box">
 				<form>
@@ -80,7 +87,6 @@ export default function Search() {
 								value={userSearch}
 								onChange={handleChange}
 							/>
-
 							<button
 								className="submit-button"
 								type="submit"
@@ -90,9 +96,6 @@ export default function Search() {
 							</button>
 						</div>
 					</div>
-
-      <button onClick={handleStart}>Start</button></div>}
-
 				</form>
 				{user.username && (
 					<div className="found msg">
@@ -116,7 +119,6 @@ export default function Search() {
 					</div>
 				)}
 			</div>
-
 
 			{/* <div className="share-link">
                 <a>Or send registration Link </a><a><SendIcon /></a>
