259b964b856b672f0a987eb6059d4338d758facb
Daggiana Madrid Andrade <daggianama@gmail.com>
2023-06-19 15:47:00 +0200

    solved bug on pending invites

--- client/src/pages/PendingInvites.jsx
+++ client/src/pages/PendingInvites.jsx
@@ -2,12 +2,10 @@
 import { useState, useContext,  useEffect } from 'react'
 import { useNavigate  } from 'react-router-dom'
 import api from '../services/data'
-import AuthContext from "../contexts/AuthContext";
+// import AuthContext from "../contexts/AuthContext";
 
 
 export default function PendingInvites() {
-    const auth = useContext(AuthContext);
-    
     var user_id = localStorage.getItem("user_id");
     const [pendingInvites, setPendingInvites] = useState([]);
    
@@ -37,14 +35,13 @@ export default function PendingInvites() {
     //     } catch (error) {
     //         console.error("Error accepting invitation", error);
     //     }
-        navigate(`/invitation/${pendingInvites.hash}`);
-        navigate(`/event/123444`);
+        navigate(`/event/${pendingInvites.hash}`);
     }
 
     return (
         <div className='pending-page'>
             <h3>Pending Invitations</h3>
-            {pendingInvites ? 
+            {pendingInvites && pendingInvites.invitee ? 
                     <div className='invitation-card'>
                         <p><strong>{pendingInvites.invitee.username}</strong> has invited you to match a date</p>
                         
