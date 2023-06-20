7323c1c2ead874fc27b385ce24b65d6de7ae6e6c
the-swedish-one <a.damgaard@oxon.org>
2023-06-20 12:23:32 +0200

    Conditionally render page if there is event_hash

--- client/src/pages/Match.jsx
+++ client/src/pages/Match.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
 import api from "../services/data.js";
+import Map from "../components/Map";
 
 export default function Match() {
   const scrollReference = useRef(null);
   const [plan, setPlan] = useState({});
+  const [hash, setHash] = useState("");
 
-  // use url params to get plan ID
-  // const { hash } = useParams();
+  //get the hash from local storage
+  setHash(localStorage.getItem("event_hash"));
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,36 +38,45 @@ export default function Match() {
 
   return (
     <div>
-      <div>
-        <h1>It's a Date!</h1>
+      {hash ? (
+        <div>
+          <div>
+            <img src="../assets/match-title.png" alt="It's a Date!" />
+            <h1>It's a Date!</h1>
 
-        <div className="featured-date">
-          <div className="image-card">
-            <img className="match-image" src={plan.imageSrc} />
+            <div className="featured-date">
+              <div className="image-card">
+                <img className="match-image" src={plan.imageSrc} />
+              </div>
+
+              <div className="featured-text">
+                <h2>You both chose: {plan.name}</h2>
+                <p className="match-longDesc">{plan.longDescription}</p>
+              </div>
+            </div>
+
+            <button className="btn-readMore" onClick={readMore}>
+              Read more
+            </button>
           </div>
 
-          <div className="featured-text">
-            <h2>You both chose: {plan.name}</h2>
-            <p className="match-longDesc">{plan.longDescription}</p>
+          <div ref={scrollReference}>
+            {plan.searchKeyword ? (
+              <div>
+                <h2>Check out some useful places for your date</h2>
+                <img className="match-image" src={plan.imageSrc} />
+                <Map />
+              </div>
+            ) : null}
           </div>
         </div>
-
-        <button className="btn-readMore" onClick={readMore}>
-          Read more
-        </button>
-      </div>
-
-      <div ref={scrollReference}>
-        {plan.keyword ? (
-          <div>
-            <h2>Check out some useful places for your date</h2>
-
-            {/* placeholder imge to prove the scroll works */}
-            <img className="match-image" src={plan.imageSrc} />
-            {/* <div>  ////  MAP   ////   </div> */}
-          </div>
-        ) : null}
-      </div>
+      ) : (
+        <div className="no-event">
+          <h2>There is no event to show</h2>
+          <p>Go to the home page and create a new event</p>
+          <button>Create event</button>
+        </div>
+      )}
     </div>
   );
 }
