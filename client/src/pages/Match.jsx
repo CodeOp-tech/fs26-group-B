45941837183db389ac45b01a808e4c7128dd9763
the-swedish-one <a.damgaard@oxon.org>
2023-06-21 13:41:51 +0200

    Check if event is closed and belongs to user

--- client/src/pages/Match.jsx
+++ client/src/pages/Match.jsx
@@ -8,6 +8,10 @@ export default function Match() {
   const navigate = useNavigate();
   const scrollReference = useRef(null);
   const [plan, setPlan] = useState({});
+  const [event, setEvent] = useState({});
+  const [user, setUser] = useState({});
+  const [eventShouldBeDisplayed, setEventShouldBeDisplayed] = useState(false);
+  const [userIsInviter, setUserIsInviter] = useState(false);
 
   //get the eventId from url params
   const { event_id } = useParams();
@@ -15,6 +19,21 @@ export default function Match() {
   useEffect(() => {
     const fetchData = async () => {
       const event = await api.getEventById(event_id);
+      setEvent(event);
+
+      const user = await api.getMyProfile();
+      setUser(user);
+
+      console.log(event.inviter.id);
+
+      if (
+        event.status === false &&
+        (event.inviter.id === user.id || event.invitee.id === user.id)
+      )
+        setEventShouldBeDisplayed(true);
+
+      if (event.inviter.id === user.id) setUserIsInviter(true);
+
       const planID = event.chosenPlanId;
       // console.log(planID);
 
@@ -26,6 +45,10 @@ export default function Match() {
 
   function goHome() {
     navigate("/home");
+  }
+
+  function goNotifications() {
+    navigate("/notifications");
   }
 
   // function to scroll the page down to the map
@@ -42,10 +65,14 @@ export default function Match() {
 
   return (
     <div>
-      {event_id ? (
+      {eventShouldBeDisplayed ? (
         <div>
-          <div>
-            {/* <img src="../assets/match-title.png" alt="It's a Date!" /> */}
+          <div className="top-window">
+            <img
+              src="./assets/match-title.png"
+              alt="It's a Date!"
+              className="match-title"
+            />
             <h1>It's a Date!</h1>
 
             <div className="featured-date">
@@ -54,7 +81,11 @@ export default function Match() {
               </div>
 
               <div className="featured-text">
-                <h2>You both chose: {plan.name}</h2>
+                <h2>
+                  You and{" "}
+                  {userIsInviter ? event.invitee.name : event.inviter.name}{" "}
+                  chose: {plan.name}
+                </h2>
                 <p className="match-longDesc">{plan.longDescription}</p>
               </div>
             </div>
@@ -66,7 +97,7 @@ export default function Match() {
 
           <div ref={scrollReference}>
             {plan.searchKeyword ? (
-              <div>
+              <div className="map-div">
                 <h2>Check out some useful places for your date</h2>
                 {/* <img className="match-image" src={plan.imageSrc} /> */}
                 <Map />
@@ -76,9 +107,15 @@ export default function Match() {
         </div>
       ) : (
         <div className="no-event">
-          <h2>There is no event to show</h2>
-          <p>Go to the home page and create a new event</p>
-          <button onClick={goHome}>Create event</button>
+          <h2>This event doesn't have a match yet 😢</h2>
+          <p>
+            Go to the home page and create a new event or check your
+            notifications to see if you have an invitation!
+          </p>
+          <button className="btn-home" onClick={goHome}>
+            Create event
+          </button>
+          <button onClick={goNotifications}>Notifications</button>
         </div>
       )}
     </div>
