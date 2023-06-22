0b0d8a6e30d1328db5c38ac7d7a029b882fd354e
the-swedish-one <a.damgaard@oxon.org>
2023-06-22 11:30:25 +0200

    Add pusher for notifications

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,9 +6,12 @@ import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import NotificationsNoneRoundedIcon from "@mui/icons-material/NotificationsNoneRounded";
 import NotificationsActiveRoundedIcon from "@mui/icons-material/NotificationsActiveRounded";
 import AuthContext from "../contexts/AuthContext";
+import Pusher from "pusher-js";
 
 // need endpoint to get open events for user
 
+const PUSHER_KEY = import.meta.env.VITE_PUSHER_KEY;
+let channel;
 export default function NavBar() {
   const auth = useContext(AuthContext);
   const [selectSignUp, setSelectSignUp] = useState(false);
@@ -16,22 +19,51 @@ export default function NavBar() {
   const navigate = useNavigate();
   const [pendingInvites, setPendingInvites] = useState([]);
   const [isNotification, setIsNotification] = useState(false);
+  const [user, setUser] = useState({});
   //const navigate = useNavigate();
 
   useEffect(() => {
     auth.user && fetchData();
-  }, [auth.user]);
+
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher(PUSHER_KEY, {
+      cluster: "eu",
+      forceTLS: true,
+    });
+
+    if (user?.id && !channel) {
+      channel = pusher.subscribe(`user-${user.id}`);
+      channel.bind("invitation", function (data) {
+        console.log(data);
+        alert(JSON.stringify(data));
+        setIsNotification(true);
+      });
+    }
+  }, [auth.user, user]);
 
   const fetchData = async () => {
     try {
       const data = await api.getOpenEvents();
       data && setPendingInvites(data);
+      console.log("data is: ", data);
     } catch (error) {
       console.error("Error fetching open events", error);
     }
-    pendingInvites ? setIsNotification(true) : setIsNotification(false);
+    pendingInvites.length > 0
+      ? setIsNotification(true)
+      : setIsNotification(false);
     console.log(pendingInvites);
   };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  async function getProfile() {
+    const user = await api.getMyProfile();
+    setUser(user);
+  }
 
   const handleSelectSignUp = () => {
     setSelectSignUp(true);
